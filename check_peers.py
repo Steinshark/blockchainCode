621ecb877f15b85c71f411c9d868cfe8a13d76e7
steinshark <everettperson@gmail.com>
2022-01-25 23:03:38 -0500

    color coding

--- check_peers.py
+++ check_peers.py
@@ -1,11 +1,11 @@
 from blockchain_utilities import *
-
+from BlockchainErrors import *
 
 hosts = [line.strip() for line in open('hosts.txt').readlines()]
 ports = ["5000","5001"]
 
 for host in hosts:
-    print(f"attempting to access host: {host} ")
+    print(f"{Colors.OKBLUE}attempting to access host: {host}{Colors.ENDC}")
     for port in ports:
         print(f"on port {port}")
         try:
@@ -17,7 +17,7 @@ for host in hosts:
 
         try:
             verify_blockchain(blockchain_download)
-            print("blockchain verified!")
+            print(f"{Colors.OKBLUE}blockchain verified!{Colors.ENDC}")
         except BlockChainVerifyError as b:
             print(b)
             continue
