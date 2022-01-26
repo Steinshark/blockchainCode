069d94368b7cb3b7e6f180d2f6e8c92581b276a6
steinshark <everettperson@gmail.com>
2022-01-25 23:43:56 -0500

    test

--- BlockchainUtilities.py
+++ BlockchainUtilities.py
@@ -49,7 +49,7 @@ def get_blockchain(hostname='cat',port='5000'):
             hash_to_next_block = retrieve_block_hash(next_block_as_JSON)
         except HashRetrievalException as h:
             raise BlockChainRetrievalError(str(h))
-    if hostname == 'gopher':
+    if hostname == 'grizzly':
         print(blockchain)
     return blockchain
 
