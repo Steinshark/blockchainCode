ab2b294c28a34c1920535d8571f703f62de257fb
steinshark <everettperson@gmail.com>
2022-01-25 23:01:04 -0500

    colors take 2

--- BlockchainErrors.py
+++ BlockchainErrors.py
@@ -2,17 +2,17 @@
 ####################################  NETWORK ERRORS  ###################################
 #########################################################################################
 class Color:
-    self.HEADER = '\033[95m'
-    self.OKBLUE = '\033[94m'
-    self.OKCYAN = '\033[96m'
-    self.OKGREEN = '\033[92m'
-    self.WARNING = '\033[93m'
-    self.FAIL = '\033[91m'
-    self.ENDC = '\033[0m'
-    self.BOLD = '\033[1m'
-    self.UNDERLINE = '\033[4m'
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
 
-    
+
 class ConnectionException(Exception):
     def __init__(self,msg):
         l = super()
