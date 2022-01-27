22e7f1c1f4d708f9946ea57bc60533fb5b873fc9
steinshark <everettperson@gmail.com>
2022-01-27 16:39:58 -0500

    add test features

---
+++ test.py
@@ -0,0 +1,9 @@
+from BlockchainUtilities import *
+from BlockTools import *
+from BlockChainErrors import *
+from server import *
+from argparse import *
+
+
+if __name__ == "__main__":
+    get_blockchain()
