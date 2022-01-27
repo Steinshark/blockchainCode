07451dd01929ff69c189cbeeee0ba46511406533
Everett Stenberg <m226252@usna.edu>
2022-01-27 08:44:57 -0500

    server fixes

--- BlockTools.py
+++ BlockTools.py
@@ -16,7 +16,7 @@ def retrieve_head_hash(host="cat",port="5000",timeout=5):
     url = f"http://{host}:{port}/head"
 
     try:
-        if hostname == 'lion':
+        if host == 'lion':
             print(f"askign for {url}")
             print(f"got {get(url,timeout=timeout).content}")
         return get(url,timeout=timeout).content.decode()
