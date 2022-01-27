be3aff94dc0eaa92715969d33fdea2af7468d9a0
steinshark <everettperson@gmail.com>
2022-01-27 16:28:04 -0500

    fix bugs

--- BlockchainUtilities.py
+++ BlockchainUtilities.py
@@ -48,6 +48,8 @@ def get_blockchain(hostname='cat',port='5000'):
             this_block_as_JSON = retrieve_block(this_hash,host=hostname,port=port)
             this_block = JSON_to_block(this_block_as_JSON)
             next_hash = retrieve_block_hash(this_block_as_JSON)
+            if next_hash == '':
+                break
             print(f"next hash is: {next_hash}")
             next_block = JSON_to_block(retrieve_block(next_hash,host=hostname,port=port))
 
