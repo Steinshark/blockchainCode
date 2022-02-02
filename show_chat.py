596256c7943103146478972d1f8ec3ebb884df48
Steinshark <everettperson@gmail.com>
2022-02-01 20:58:15 -0500

    updated showchat behavior

--- show_chat.py
+++ show_chat.py
@@ -1,24 +1,53 @@
-from BlockchainUtilities import get_blockchain, verify_blockchain
+from BlockchainUtilities import *
+from BlockchainErrors import *
 import argparse
 
+class ChatService:
+    def __init__():
+        format_parser()
+        self.host = self.args.host
+        self.port = self.args.port
+        self.blockchain_check = True
+
+    def format_parser(self):
+        self.parser = argparse.ArgumentParser(description='Specify your own hostname and port',prefix_chars='-')
+        self.parser.add_argument('--host',metavar='host',required=False, type=str,help='specify a hostname',default="http://cat")
+        self.parser.add_argument('--port',metavar='port',required=False, type=str,help='specify a port',default='5000')
+        self.args = self.parser.parse_args()
+
+
+    def download_blockchain(self):
+        try:
+            self.blockchain_download = get_blockchain(self.host,self.port)
+        except BlockChainError as b:
+            print(b)
+            print(f"{Color.RED}Error Downloading Blockchain: Terminated{Color.END}")
+            self.blockchain_download = None
+            self.blockchain_check = False
+
+
+    def verify_blockchain(self):
+        if self.blockchain_download is None:
+            print(f"{Color.RED}No blockchain found{Color.END}")
+            return
+        try:
+            self.blockchain_len = verify_blockchain(self.blockchain_download)
+        except BlockChainVerifyError as b:
+            print(b)
+            print(f"{Color.RED}Error Verifying Blockchain{Color.END}")
+
+
+
 if __name__ == '__main__':
-    #args
-    parser = argparse.ArgumentParser(description='Specify your own hostname and port',prefix_chars='-')
-    parser.add_argument('--host',metavar='host',required=False, type=str,help='specify a hostname',default="http://cat")
-    parser.add_argument('--port',metavar='port',required=False, type=str,help='specify a port',default='5000')
-    args = parser.parse_args()
-    host,port = (args.host,args.port)
 
-    # Download the blockchain and verify the blocks
-    blockchain = get_blockchain(host,port)
+    # Create an instance of the class
+    instance = ChatService()
 
-    # Dont enter verify_blockchain if we had a connection error
-    blockchain_failed = blockchain is None or not verify_blockchain(blockchain)
+    # Format the arguments
+    instance.format_parser()
 
-    # Print the chat log
-    if blockchain_failed:
-        print(blockchain)
-        exit()
+    # Try to download the blockchain
+    instance.download_blockchain()
 
-    for hash,block in blockchain:
-        print(block['payload']['chat'])
+    # Attempt to verify the blockchain
+    instance.verify_blockchain()
