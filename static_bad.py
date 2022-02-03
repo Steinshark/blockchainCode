0d91fdb21afa4ae7b68595cbb7d525e0ccce5615
Everett Stenberg <m226252@usna.edu>
2022-02-03 08:50:18 -0500

    fix static servers

--- static_bad.py
+++ static_bad.py
@@ -2,5 +2,5 @@ from ServerSource import *
 from BlockTools import *
 
 if __name__ == "__main__":
-    s = Server()
+    s = StaticServer()
     s.run(host='lion',port=5001,gen_block=build_block('',{'chat' : 'a BAD server >:('},-4000000))
--- static_good.py
+++ static_good.py
@@ -1,5 +1,5 @@
 from ServerSource import *
 from BlockTools import *
 if __name__ == "__main__":
-    s = Server()
+    s = StaticServer()
     s.run(host='lion',port=5000,gen_block=build_block('',{'chat' : 'a good server indeed'},0))
