43f8ef000f13f0f9b0c4942e3b713e038b915d8b
Steinshark <everettperson@gmail.com>
2022-02-02 19:07:28 -0500

    v

--- BlockchainUtilities.py
+++ BlockchainUtilities.py
@@ -70,9 +70,9 @@ def get_blockchain(hostname='cat',port='5000',caching=False,cache_location='cach
 
         # verify the block
         print(f"checking {block_hash[:10]} on {block}")
-        hashed_to = hash('hex',loads(retrieve_block(retrieve_prev_hash(block),host=hostname,port=port).encode()))
+        hashed_to = hash('hex',retrieve_block(retrieve_prev_hash(block),host=hostname,port=port).encode())
         print(f"hashed to {hashed_to}")
-        check = check_fields(block,block_hash,allowed_versions=[0],allowed_hashes=['',block_hash],trust=trust)
+        check = check_fields(block,block_hash,allowed_versions=[0],allowed_hashes=['',hashed_to],trust=trust)
         if check:
             # add it to the chain
             blockchain.insert(0,(hash,block))
