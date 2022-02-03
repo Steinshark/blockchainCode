998d8bfb5bb2a527314efe2151ea7d6e2a5dc404
Everett Stenberg <m226252@usna.edu>
2022-02-03 09:11:47 -0500

    ;push

--- send_chat.py
+++ send_chat.py
@@ -1,7 +1,7 @@
 from blockchain_utilities import http_get, http_post, build_block
 
 
-if not __name__ == "__main__":
+if __name__ == "__main__":
     URL = 'http://lion:5002'
 
     # Get user message
@@ -13,28 +13,11 @@ if not __name__ == "__main__":
 
     # Build a new block and send it to the blockchain
     json_encoded_block = build_block(head_hash.decode(),{'chat' : message},0)
+    print(f"sending {json_encoded_block} of type {type(json_encoded_block)}")
+
     push_data = {'block' : json_encoded_block}
     post = http_post(URL+'/push',push_data)
     print(post.status_code)
 
-
-else:
-    msg = 'hello world'
-
-
-    URL = 'http://lion:5002'
-
-    # Get user message
-    for m in sends:
-        message = m
-
-        # Get the head blocks hash
-        request_url = URL+'/head'
-        head_hash = http_get(request_url).content
-
-        # Build a new block and send it to the blockchain
-        json_encoded_block = build_block(head_hash.decode(),{'chat' : message},0)
-        print(f"sending {json_encoded_block} of type {type(json_encoded_block)}")
-        push_data = {'block' : json_encoded_block}
-        post = http_post(URL+'/push',push_data)
+ta)
         print(post.status_code)
