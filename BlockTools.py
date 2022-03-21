7f279a26589c78fcee01f888dc699eeef0342310
Everett Stenberg <m226252@usna.edu>
2022-03-21 16:20:03 -0400

    checking

--- BlockTools.py
+++ BlockTools.py
@@ -144,7 +144,7 @@ def iter_local_chain(block_hash,known_chains,version=0):
 
 # given a processed block (python dictionary), check the block for keys, then check
 # key values using the named parameters
-def check_block(block,block_string,allowed_versions=[0],allowed_hashes=[''],trust=False,diff=6,check_tx=False):
+def check_block(block,block_string,allowed_versions=[0],allowed_hashes=[''],trust=False,diff=6,check_tx=True):
     block_hash = sha_256_hash(block_string.encode())
 
     if trust:
